b5029ef4e09c96b764b22b943b89548568eae6d5
soujanya09 <reddeboinasoujanya@gmail.com>
2026-05-05 19:38:47 +0530

    initial commit

---
+++ paymentService/src/test/java/com/pgManagement/paymentService/PaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pgManagement.paymentService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
